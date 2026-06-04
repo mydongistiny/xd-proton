0621da5753378271d2d5985e3aac4c5c15444c32
Vyrolian <107125437+Vyrolian@users.noreply.github.com>
2026-06-04 04:37:22 +0300

    Remove winepulse fast polling and throttling patch
    Signed-off-by: Jason Edson <jaysonedson@gmail.com>

--- patches/protonprep-valve-staging.sh
+++ patches/protonprep-valve-staging.sh
@@ -264,9 +264,6 @@ apply_all_in_dir() {
 
 ### (2-7) PROTON-GE ADDITIONAL CUSTOM PATCHES ###
 
-    echo "WINE: Add winepulse fast polling env variable"
-    apply_patch "../patches/proton/winepulse-fast-polling.patch"
-
     echo "WINE: Add an env variable to override channel count in winealsa"
     apply_patch "../patches/proton/winealsa-override-channel-count.patch"
 
