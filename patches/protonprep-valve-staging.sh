7536914750208efd1eb15994a5c0dca432c986a6
GloriousEggroll <gloriouseggroll@gmail.com>
2026-07-22 00:37:04 -0600

    patches: add workaround for steamrt4 old gnutls bug.
    Signed-off-by: Jason Edson <jaysonedson@gmail.com>

--- patches/protonprep-valve-staging.sh
+++ patches/protonprep-valve-staging.sh
@@ -240,6 +240,9 @@ apply_all_in_dir() {
     echo "WINE: -HOTFIX- Validate Winsock connect address arguments"
     apply_patch "../patches/wine-hotfixes/pending/ws2_32-validate-connect-address.patch"
 
+    echo "WINE: -HOTFIX- Fall back when GnuTLS lacks NO_SHUFFLE_EXTENSIONS"
+    apply_patch "../patches/wine-hotfixes/pending/secur32-fallback-without-no-shuffle-extensions.patch"
+
 ### END WINE HOTFIX/BACKPORT SECTION ###
 
 ### (2-6) WINE PENDING UPSTREAM SECTION ###
